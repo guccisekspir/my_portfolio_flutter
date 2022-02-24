64bdedd13173fb5d8d15699219309c3300763e7b
Gucci Şekspir <348275@ogr.ktu.edu.tr>
2022-02-24 16:57:00 +0300

    Create gradientList.dart

---
+++ lib/consts/gradientList.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+List<List<Color>> gradientLinks = [
+  [const Color(0XFF2193b0), const Color(0XFF6dd5ed)],
+  [const Color(0XFFcc2b5e), const Color(0XFF753a88)],
+  [const Color(0XFFeb3349), const Color(0XFFf45c43)],
+  [const Color(0XFF02aab0), const Color(0XFF00cdac)],
+  [const Color(0XFFFFE53B), const Color(0XFFFF2525)],
+  [const Color(0XFF4568dc), const Color(0XFFb06ab3)],
+  [const Color(0XFFed4264), const Color(0XFFffedbc)],
+  [const Color(0XFF4158D0), const Color(0XFFC850C0), const Color(0XFFFFCC70)],
+];
